a81e6c34a214b8fa80e0bfbc309d2a916b995bee
gvsreekar <80467534+gvsreekar@users.noreply.github.com>
2024-06-22 13:19:15 +0530

    Working loaded_model

--- predictive_system.py
+++ predictive_system.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import joblib 
-from custom_transformers import column_names, yeojohnson, boxcox
+from custom_transformers import column_names
 
 loaded_model = joblib.load('rfc_model_final.joblib')
 
@@ -10,4 +10,4 @@ input_data = pd.DataFrame([{'Age':25,'Gender':1,'City':'C20',
                             'Grade':2,'Total Business Value':940839,'Quarterly Rating':3,
                             'income_increased':0,'rating_increased':1}])
 
-print(loaded_model.predict(input_data))
+print(f" The churn is {loaded_model.predict(input_data)}")
